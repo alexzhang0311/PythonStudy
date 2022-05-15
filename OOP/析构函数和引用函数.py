9c75e31555b8ef6663a73a15d2fb2c61e5d031fc
Alex <317601060@qq.com>
2022-05-15 16:29:11 +0800

    python oop git test

---
+++ OOP/析构函数和引用函数.py
@@ -0,0 +1,20 @@
+class Person(object):
+    def __init__(self) -> None:
+        self.name = 'alex'
+        print("这是构造函数")
+        self.fp = open('xx.txt','w')
+    
+    def greet(self):
+        print("hello {}".format(self.name))
+
+    def write(self,message):
+        self.fp.write(message)
+    
+    ###内存释放前会调用的函数###
+    def __del__(self):
+        print("这是析构函数")
+        self.fp.close()
+
+p = Person()
+p.greet()
+p.write('hello alex')
